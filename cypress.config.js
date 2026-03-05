a296721a79a4da3fa67279b2c2d302fdf26fe67d
Andrii <cesaaar2007@gmail.com>
2026-03-06 00:45:21 +0200

    Config settings

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: true,
+  videoCompression: false,
+  defaultCommandTimeout: 5000,
+  pageLoadTimeout: 20000,
+  retries: {
+    runMode: 1,
+    openMode: 1
+  },
 
   e2e: {
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
